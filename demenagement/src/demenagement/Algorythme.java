689f9fa4ecdb3da77d68bd88212b579b065120fa
Mehdi <mehdi.chaouche@le-campus-numerique.fr>
2020-12-08 08:46:52 +0100

    Config OK | Demenagement OK |

--- demenagement/src/demenagement/Algorythme.java
+++ demenagement/src/demenagement/Algorythme.java
@@ -1,25 +1,18 @@
 package demenagement;
 
 public class Algorythme {
-	public static void main(String[] args)  {
+	public static void main(String[] args) {
 		int quantite_ancien_local = 34;
 		int caisses_en_transport = 0;
 		int caisses_max_transport = 9;
-		int caisses_max_camion1 = 9;
-		while (quantite_ancien_local > 0)
-		{
+		while (quantite_ancien_local > 0) {
 			System.out.println(" ");
-			while (caisses_en_transport < caisses_max_transport && quantite_ancien_local > 0)
-			{
+			while (caisses_en_transport < caisses_max_transport && quantite_ancien_local > 0) {
 				quantite_ancien_local--;
 				caisses_en_transport++;
-				if (caisses_en_transport < caisses_max_camion1)
-				{
-				}
 			}
 			System.out.print("|Un voyage de " + caisses_en_transport + " cartons.|");
-			while (caisses_en_transport > 0)
-			{
+			while (caisses_en_transport > 0) {
 				caisses_en_transport--;
 			}
 		}
